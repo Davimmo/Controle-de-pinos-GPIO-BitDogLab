ab72dd90b964ba264663e5a75df21aa831220651
Davi BC <davi20cavalcanti@gmail.com>
2025-01-19 21:32:10 -0300

    Adicionando as funcionalidades do buzzer implementadas por Yuri

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
 
+#define BUZZER_PIN 21 //Porta associada ao Buzzer
 #define linhas 4  // Definindo Linhas da Matriz
 #define colunas 4 // Definindo colunas da Matriz
 
@@ -64,6 +66,27 @@ char read_keypad()
         gpio_put(PINOS_DA_LINHA[linha], 0);
     }
     return '\0';
+}
+
+void buzz(uint freq, uint tempo) {
+
+    uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
+    uint channel = pwm_gpio_to_channel(BUZZER_PIN);
+
+    // Configurando a frequência
+    uint32_t clock_freq = 125000000; 
+    uint32_t divider = clock_freq / freq / 65536 + 1; 
+    uint32_t top = clock_freq / (divider * freq);
+
+    // Configurando as repetições
+    pwm_set_clkdiv(slice_num, divider); 
+    pwm_set_wrap(slice_num, top - 1);
+    pwm_set_chan_level(slice_num, channel, top / 2);
+    pwm_set_enabled(slice_num, true);
+
+    sleep_ms(tempo);
+
+    pwm_set_enabled(slice_num, false);
 }
 
 int main()
@@ -166,6 +189,9 @@ int main()
                 gpio_put(LED_G, 0);
                 break;
             case '#':
+                 gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
+                 buzz(550, 500); //Frequência de 550Hz por um tempo de 500ms
+                 sleep_ms(500);
 
                 // Buzzers
                 
